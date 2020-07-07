2ad58d5abaed543540eca5a73724970d4ad3e350
Luke Walsh <lukewalsh@lwalsh-mbpr19.local>
2020-07-06 17:49:50 -0700

    Pull api_key into variable

--- src/api.js
+++ src/api.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 
+var api_key = "RGAPI-1025294b-5ba1-4def-a7cf-e697794daad5"
+
 export default {
   getSummonerInfo (summonerName) {
     return axios({
       method: 'get',
-      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=RGAPI-038eaa59-ac10-4b7c-8b56-56c5ebbac9cd'
+      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName + '?api_key=' + api_key
     })
   },
 
   getSummonerMatches (accountId) {
     return axios({
       method: 'get',
-      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountId + '?api_key=RGAPI-038eaa59-ac10-4b7c-8b56-56c5ebbac9cd'
+      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountId + '?api_key=' + api_key
     })
   },
 
   getMatch(matchId) {
     return axios({
       method: 'get',
-      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/match/v4/matches/' + matchId + '?api_key=RGAPI-038eaa59-ac10-4b7c-8b56-56c5ebbac9cd'
+      url: 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/match/v4/matches/' + matchId + '?api_key=' + api_key
     })
   }
 }
